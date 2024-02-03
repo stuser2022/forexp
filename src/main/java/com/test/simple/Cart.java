a25ed02df39b91291e68cf7b180fb225b6f6e4bd
SoftwareTestingo <softwaretestingblog.in@gmail.com>
2024-02-03 20:02:49 +0530

    Cart Page added

---
+++ src/main/java/com/test/simple/Cart.java
@@ -0,0 +1,10 @@
+package com.test.simple;
+
+public class Cart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Cart Page Added");
+	}
+
+}
