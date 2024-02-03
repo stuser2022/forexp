8dd8f7e25becd57cccdfef1ba3e6f8ec904ac045
SoftwareTestingo <softwaretestingblog.in@gmail.com>
2024-02-03 16:36:38 +0530

    Added

---
+++ src/main/java/com/test/simple/Simple.java
@@ -0,0 +1,11 @@
+package com.test.simple;
+
+public class Simple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Best");
+		System.out.println("log");
+	}
+
+}
